f356f68bf220fbce86ad72e628e0aa2b5c83189b
Dan Rigsby <danrigsby@gmail.com>
2016-06-21 13:56:53 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -101,11 +101,14 @@ which -s scala || brew install scala
 which -s node || brew install node
 which -s wget || brew install wget
 which -s ack || brew install ack
+which -s z || brew install z
+which -s figlet || brew install figlet
 brew install python
 
 # Install web/javascript tooling
 which -s gulp || npm install gulp -g
 which -s grunt || npm install grunt -g
+which -s flow || brew install flow
 
 # Change git autocrlf to "input"
 git config --global core.autocrlf input
@@ -121,6 +124,7 @@ printf '%s\n%s' '[storage]' 'engine = google_drive' >> .mackup.cfg
 # Instll quick look plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package && qlmanage -r
 
+brew cask install alfred
 brew cask install atom
 brew cask install calibre
 brew cask install dashlane
@@ -130,47 +134,52 @@ brew cask install firefox
 brew cask install fluid
 brew cask install google-chrome
 brew cask install google-drive
-brew cask install iterm2
+brew cask install iterm3
 brew cask install openemu
 brew cask install sequel-pro
 brew cask install sourcetree
 brew cask install virtualbox
 brew cask install visual-studio-code
 brew cask install vlc
-brew cask install xtrafinder
+brew cask install gitkraken
+brew cask install bartender
+brew cask install kindle
+brew cask install stackato
+brew cask install discord
+brew cask install duet
+brew cask install slack
 # brew cask install steam
 
 brew cask install bartender
 brew cask install cheatsheet
 
 #Install atom plugin
-  apm install autocomplete-paths
-  apm install editorconfig
-  apm install enhanced-tabs
-  apm install file-icons
-  #apm install git-blame
-  apm install git-go
-  apm install git-history
-  apm install git-log
-  apm install git-plus
-  #apm install git-projects
-  apm install highlight-line
-  apm install highlight-selected
-  apm install language-scala
-  apm install linter
-  apm install linter-eslint
-  apm install merge-conflicts
-  apm install minimap
-  apm install minimap-color-highlight
-  apm install minimap-git-diff
-  apm install minimap-selection
-  apm install open-recent
-  apm install pigments
-  apm install project-manager
-  apm install react
-  apm install regex-railroad-diagram
-  apm install sort-lines
-  apm install sublime-style-column-selection
+apm install autocomplete-paths
+apm install editorconfig
+apm install enhanced-tabs
+apm install file-icons
+apm install git-blame
+apm install git-go
+apm install git-history
+apm install git-log
+apm install git-plus
+apm install highlight-line
+apm install highlight-selected
+apm install language-scala
+apm install linter
+apm install linter-eslint
+apm install merge-conflicts
+apm install minimap
+apm install minimap-git-diff
+apm install minimap-selection
+apm install open-recent
+apm install pigments
+apm install project-manager
+apm install react
+apm install regex-railroad-diagram
+apm install sort-lines
+apm install sublime-style-column-selection
+apm install sync-settings
 
 # Cleanup
 brew cleanup
